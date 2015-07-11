e7ebbaa6545898733f127b41119bf5423f6e3d61
weizhou <weizhou@mail.ustc.edu.cn>
2015-07-11 22:24:37 +0800

    make it better to use

---
+++ multiget.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+n="$#"
+
+if [ "$n" = 0 ]; then
+    echo "Usage: ./multiget.sh url... numOfImages"
+    exit 1
+fi
+
+for ((i=1;i<n;i++))
+do
+    eval url='$'"$i"
+    ./pagesget.sh $url &
+done
+
+sleep 600
+
+eval numOfImages='$'"$n"
+time ./killwget.sh $numOfImages
+
+exit 0
